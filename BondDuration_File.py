007b06c9683b542ee0e0e6b37a0966be6b26a6f7
xl3363-creator <xl3363@columbia.edu>
2025-10-05 19:40:50 -0400

    Update BondDuration_File.py

--- BondDuration_File.py
+++ BondDuration_File.py
@@ -1,3 +1,22 @@
+def getBondDuration(y,face,couponRate,m,ppy=1):
+    y1 = y/ppy
+    m1 = m * ppy
+    cf = couponRate * face / ppy
+    pvcfsum = 0
+    weightedsum = 0
+    for i in range(m1):
+        pvm = (1+y1)**-(i+1)
+        pvcf = pvm * cf
+        pvcfsum = pvcf + pvcfsum
+        weightedsum = pvcf * (i+1) + weightedsum
+    totalbondprice = pvcfsum + face*(1+y1)**-m1
+    totalweighted = weightedsum + m1*face*(1+y1)**-m1
+    bondduration = totalweighted/totalbondprice
+    return bondduration
 
-def getBondDuration(y, face, couponRate, m, ppy = 1):
-    return(8.51)
+y = 0.03
+face = 2000000
+couponRate = 0.04
+m = 10
+ppy = 2
+getBondDuration(y,face,couponRate,m,ppy=1)
