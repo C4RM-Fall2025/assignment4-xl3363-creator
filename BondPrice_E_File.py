6c933281645f6cfb3191d8adaa280938d8d08c08
xl3363-creator <xl3363@columbia.edu>
2025-10-05 19:49:15 -0400

    Update BondPrice_E_File.py

--- BondPrice_E_File.py
+++ BondPrice_E_File.py
@@ -1,10 +1,10 @@
-def getBondPrice_E(face, couponRate, m, yc):
-    m = len(yc)
-    bondprice = 0
-    for t,y in enumerate (yc,start=1):
-        cf = face * couponRate if t < m else face * (1+couponRate)
-        pvm = 1/(1+y)**(t)
-        pvcf = cf*pvm
-    bondPrice = bondprice + pvcf
-    
-    return(bondPrice)
+def getBondPrice_E(face, couponRate, yc):
+    m=len (yc)
+    bondPrice = 0
+
+    for t, y in enumerate (yc, start=1):
+        c=face*couponRate if t<m else face * (1+couponRate)
+        pv=1/((1+y)**t)
+        pvcf =c*pv
+        bondPrice+=pvcf
+    return bondPrice
